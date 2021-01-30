e9b161c7cfe4b2a623296d715f69a8d0024d6efa
Alex Blackman <alexander.blackman@gmail.com>
2021-01-31 09:54:00 +1100

    Add project files.

---
+++ HFC CardComparer/CardComparerByValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace HFC_CardComparer
+{
+    class CardComparerByValue : IComparer<Card>
+    {
+        public int Compare(Card x,  Card y)
+        {
+            if (x.Suit > y.Suit)
+                return 1;
+            else if (x.Suit < y.Suit)
+                return -1;
+            else if (x.Value > y.Value)
+                return 1;
+            else if (x.Value < y.Value)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ HFC CardComparer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HFC_CardComparer
+{
+    class Program
+    {
+        static Random random = new Random();
+        static Card RandomCard()
+        {
+            Values generatedValue = (Values)random.Next(1, 15);
+            Suits generatedSuit = (Suits)random.Next(4);
+            Card generatedCard = new Card(generatedValue,generatedSuit);
+            return generatedCard;
+        }
+
+        static void PrintCard(List<Card>myList)
+        {
+            foreach (Card card in myList)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<Card> cardList = new List<Card>();
+
+            Console.WriteLine();
+
+        }
+    }
+}
